3384b89439a7cbb135a340dc8e533f8b4f3a0057
dhanush-Clode <dhanush@clode.io>
2025-07-14 13:55:52 +0530

    latest 14

--- src/redux/RootReducer.js
+++ src/redux/RootReducer.js
@@ -3,12 +3,14 @@ import  signUpReducer  from "../pages/login/Signup/SignUpReducer";
 import  loginReducer  from "../pages/login/LoginReducer";
 import vendorProfileReducer from "../pages/vendor/reducers/VendorProfileReducer";
 import productReducer from "../pages/vendor/reducers/ProductReducer";
+import publicProductReducer from "./PublicProductReducer";
 
  const rootReducer = combineReducers({
     signUpReducer: signUpReducer,
     loginReducer: loginReducer,
     vendorProfileReducer: vendorProfileReducer,
     productReducer: productReducer,
+    publicProductReducer: publicProductReducer,
 })
 
 export default rootReducer  
